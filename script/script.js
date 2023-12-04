c8752ae2d91ebc86521939f8fa34ca7f959c9db0
avento <avento@umich.edu>
2023-12-04 09:55:53 -0500

    navbar on mobile

--- script/script.js
+++ script/script.js
@@ -9,3 +9,27 @@ window.onscroll = function() {
         header.classList.remove('navbarDark');
     }
 }
+
+function updateNavbar() {
+    const top = window.scrollY;
+    const screenWidth = window.innerWidth;
+
+    if (screenWidth <= 768) { // Adjust the breakpoint based on your mobile view
+        header.classList.add('navbarDark');
+    } else {
+        if(top >=0) {
+            header.classList.add('navbarDark');
+        }
+        else {
+            header.classList.remove('navbarDark');
+        }
+    }
+}
+
+// Initial call to set the navbar class based on the initial state
+updateNavbar();
+
+// Update the navbar when the window is resized
+window.onresize = function () {
+    updateNavbar();
+};
