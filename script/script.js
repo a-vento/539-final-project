d781bf0b9058f9fe49f709a23b8d4c9bbfb7db08
avento <avento@umich.edu>
2023-12-04 00:02:28 -0500

    mobile changes

--- script/script.js
+++ script/script.js
@@ -2,7 +2,7 @@ const header = document.querySelector('.navbar');
 
 window.onscroll = function() {
     const top = window.scrollY;
-    if(top >=100) {
+    if(top >=50) {
         header.classList.add('navbarDark');
     }
     else {
