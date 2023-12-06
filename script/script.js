3aeec10e5af0e9f3e3c807b3e12388477c0ff045
avento <avento@umich.edu>
2023-12-06 16:39:42 -0500

    code cleanup

--- script/script.js
+++ script/script.js
@@ -14,22 +14,13 @@ function updateNavbar() {
     const top = window.scrollY;
     const screenWidth = window.innerWidth;
 
-    if (screenWidth <= 768) { // Adjust the breakpoint based on your mobile view
+    if (screenWidth <= 768) { 
         header.classList.add('navbarDark');
-    } else {
-        if(top >=0) {
-            header.classList.add('navbarDark');
-        }
-        else {
-            header.classList.remove('navbarDark');
-        }
-    }
+    } 
 }
 
-// Initial call to set the navbar class based on the initial state
 updateNavbar();
 
-// Update the navbar when the window is resized
 window.onresize = function () {
     updateNavbar();
 };
