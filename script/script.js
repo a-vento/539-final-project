18f3e32180e61558b27beea32c27c05c6e3c21d8
avento <avento@umich.edu>
2023-12-06 19:59:18 -0500

    axe issues again

--- script/script.js
+++ script/script.js
@@ -1,21 +1,25 @@
 const header = document.querySelector('.navbar');
 const navheader = document.querySelector('.navbar-brand');
 const link1 = document.querySelector('#navbarSupportedContent > ul > li:nth-child(2) > a');
-const link2 = document.querySelector("#navbarSupportedContent > ul > li:nth-child(3) > a")
-const link3  = document.querySelector("#navbarSupportedContent > ul > li:nth-child(4) > a")
-const link4 = document.querySelector("#navbarSupportedContent > ul > li:nth-child(1) > a")
-window.onscroll = function() {
+const link2 = document.querySelector("#navbarSupportedContent > ul > li:nth-child(3) > a");
+const link3 = document.querySelector("#navbarSupportedContent > ul > li:nth-child(4) > a");
+const link4 = document.querySelector("#navbarSupportedContent > ul > li:nth-child(1) > a");
+const toggler = document.querySelector(".navbar-toggler");
+
+window.onscroll = function () {
     const top = window.scrollY;
-    if(top >=50) {
+    const screenWidth = window.innerWidth;
+
+    if (top >= 50) {
         header.classList.add('navbarDark');
         navheader.classList.add('navbarColor');
         link1.classList.add('navbarLinkColor');
         link2.classList.add('navbarLinkColor');
         link3.classList.add('navbarLinkColor');
         link4.classList.add('navbarLinkColor');
-
     }
-    else {
+
+    if (top < 50 ) {
         header.classList.remove('navbarDark');
         navheader.classList.remove('navbarColor');
         link1.classList.remove('navbarLinkColor');
@@ -23,27 +27,9 @@ window.onscroll = function() {
         link3.classList.remove('navbarLinkColor');
         link4.classList.remove('navbarLinkColor');
     }
-}
 
-function updateNavbar() {
-    const top = window.scrollY;
-    const screenWidth = window.innerWidth;
-
-    if (screenWidth <= 768) { 
-        header.classList.add('navbarDark');
-        navheader.classList.add('navbarColor');
-        link1.classList.add('navbarLinkColor');
-        link2.classList.add('navbarLinkColor');
-        link3.classList.add('navbarLinkColor');
-        link4.classList.add('navbarLinkColor');
-    } 
-}
-
-updateNavbar();
-
-window.onresize = function () {
-    updateNavbar();
 };
+
 
 // the following is used from https://www.w3schools.com/howto/howto_js_accordion.asp
 var acc = document.getElementsByClassName("accordion");
@@ -76,3 +62,4 @@ for (i = 0; i < acc.length; i++) {
     }
   });
 }
+
